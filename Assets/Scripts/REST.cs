03392cdc00b5e7a159bbd5c5ee7a2c3f73492702
Tomasz Wojtkiewicz <222616@edu.p.lodz.pl>
2022-07-18 23:31:53 +0200

    Read input fields
    REST query now reads data from input fields

--- Assets/Scripts/REST.cs
+++ Assets/Scripts/REST.cs
@@ -1,32 +1,48 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 using System;
+using System.Text.RegularExpressions;
+using TMPro;
 
 public class REST : MonoBehaviour
 {
-    public void signUpRequest()
+    public GameObject usernameInput;
+    public GameObject passwordInput;
+    public GameObject repeatPasswordInput;
+
+    public void SignUpRequest()
     {
+        string username = usernameInput.GetComponent<TextMeshProUGUI>().text;
+        string password = passwordInput.GetComponent<TextMeshProUGUI>().text;
+        string repeatPassword = repeatPasswordInput.GetComponent<TextMeshProUGUI>().text;
+
+        // TODO: add data validation
+
         try
         {
+            WWWForm form = new();
+            form.AddField("username", username);
+            form.AddField("password", password);
+
             string url = "http://54.93.126.7/signUp.php";
 
-            var request = UnityWebRequest.Post(url, "");
-            StartCoroutine(signUpResponse(request));
+            var request = UnityWebRequest.Post(url, form);
+            StartCoroutine(SignUpResponse(request));
         }
         catch (Exception e) { Debug.Log("ERROR : " + e.Message); }
     }
 
-    private IEnumerator signUpResponse(UnityWebRequest req)
+    private IEnumerator SignUpResponse(UnityWebRequest req)
     {
         yield return req.SendWebRequest();
         if (req.result == UnityWebRequest.Result.ConnectionError)
             Debug.Log("Network error has occured: " + req.GetResponseHeader(""));
+        else if (req.downloadHandler.text.Contains("already"))
+        {
+            Debug.LogError("User already exists!");
+        }
         else
             Debug.Log("Success " + req.downloadHandler.text);
-        byte[] results = req.downloadHandler.data;
-        Debug.Log("Second Success");
-        // Some code after success
     }
 }
