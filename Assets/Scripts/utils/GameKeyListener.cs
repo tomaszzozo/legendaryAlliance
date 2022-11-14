11c31bbf0f8dad4f6c7ed1d364bfad89efbddbcf
Tomasz Wojtkiewicz <222616@edu.p.lodz.pl>
2022-11-14 22:35:23 +0100

    On esc go back, on enter next round

---
+++ Assets/Scripts/utils/GameKeyListener.cs
@@ -0,0 +1,44 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameKeyListener : MonoBehaviour
+{
+    [SerializeField] private Canvas fieldManagerCanvas;
+    [SerializeField] private Canvas attackModeCanvas;
+    [SerializeField] private Button fieldManagerBackButton;
+    [SerializeField] private Button nextTurnButton;
+    [SerializeField] private Button attackModeCancelButton;
+    
+    
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (attackModeCanvas.enabled)
+            {
+                attackModeCancelButton.onClick.Invoke();
+            }
+            else if (fieldManagerCanvas.enabled)
+            {
+                fieldManagerBackButton.onClick.Invoke();
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (attackModeCanvas.enabled)
+            {
+                
+            }
+            else if (fieldManagerCanvas.enabled)
+            {
+                
+            }
+            else
+            {
+                nextTurnButton.onClick.Invoke();
+            }
+        }
+        
+    }
+}
